f8e3d6caa18a3ae7784c1af84f9c8d55a4d3644f
LAJARGE CYRIL <cyril.lajarge@etu.unistra.fr>
2020-12-04 18:23:46 +0100

    Added function to convert labeled image to coloured image for visualisation + function to compute neighbours indices of a certain pixel (refactoring probably needed) + beginning of main algorithm

--- alphatree.py
+++ alphatree.py
@@ -8,8 +8,30 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    helpers.display_two(images.test1, images.test1)
+    test_image = images.test1
+    #labeled_image = alpha_omegaCC(test_image, 2, 2)
+    #colouredCCs = helpers.convert_to_color(labeled_image)
+    #helpers.display_two(test_image, colouredCCs)
 
+    print(helpers.get_neigbours(test_image, 4, 4))
+
+
+'''
+def alpha_omegaCC(image, alpha, omega):
+    width = image.shape[0]
+    height = image.shape[1]
+    lbl = np.full((width, height), 0)
+    rl = np.full((width, height), np.inf)
+
+    lblval = 1
+    for i in range(width):
+        for j in range(height):
+            if(lbl[i, j] == 0):
+                lbl[i, j] = lblval
+                mincc, maxcc = image[i, j]
+                rlcrt = alpha
+                neighbors = helpers.get_neighbours(image, i, j)
+'''
 
 if __name__ == "__main__":
     main()
--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 def display(image):
@@ -14,3 +15,63 @@ def display_two(image1, image2):
     plt.imshow(image2, cmap='gray')
 
     plt.show()
+
+
+def convert_to_color(image):
+    width = image.shape[0]
+    height = image.shape[1]
+    colored_image = np.zeros((width, height, 3))
+    for i in range(width):
+        for j in range(height):
+            np.random.seed(image[i, j])
+            colored_image[i, j] = np.random.rand(3)
+    return colored_image
+
+
+def get_neigbours(image, i, j):
+    width = image.shape[0]
+    height = image.shape[1]
+    if(i < 0 or i > width-1 or j < 0 or j > height-1):
+        raise Exception(
+            'index out of range')
+    neighbors = []
+    if(i > 0 and i < width-1 and j > 0 and j < height-1):
+        neighbors.append([i, j+1])
+        neighbors.append([i+1, j])
+        neighbors.append([i, j-1])
+        neighbors.append([i-1, j])
+        return neighbors
+    if(i == 0):
+        if(j == 0):
+            neighbors.append([i+1, j])
+            neighbors.append([i, j+1])
+        elif(j == height-1):
+            neighbors.append([i+1, j])
+            neighbors.append([i, j-1])
+        else:
+            neighbors.append([i, j+1])
+            neighbors.append([i+1, j])
+            neighbors.append([i, j-1])
+        return neighbors
+    if(i == width-1):
+        if(j == 0):
+            neighbors.append([i-1, j])
+            neighbors.append([i, j+1])
+        elif(j == height-1):
+            neighbors.append([i-1, j])
+            neighbors.append([i, j-1])
+        else:
+            neighbors.append([i, j+1])
+            neighbors.append([i-1, j])
+            neighbors.append([i, j-1])
+        return neighbors
+    else:
+        if(j == 0):
+            neighbors.append([i-1, j])
+            neighbors.append([i+1, j])
+            neighbors.append([i, j+1])
+        else:
+            neighbors.append([i-1, j])
+            neighbors.append([i+1, j])
+            neighbors.append([i, j-1])
+        return neighbors
